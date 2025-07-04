80a4d18a8b4ebe54395dfe956f75e067b9963fbf
kartik singh rathore <143498869+kartikrathore7773@users.noreply.github.com>
2025-07-04 15:35:38 +0000

    Arrays Part 1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,15 @@
+//Arrays
+
+const myArr = [0, 1, 2, 3, 4, 5]
+console.log(myArr[4]);
+
+const myHeros = ["Shaktiman","Nagraj"]
+
+const myArr2 = new Array(1,2,3,4,5)
+
+console.log(myArr2[0]);
+
+//Array Methods
+
+myArr.push(6)
+console.log(myArr);
