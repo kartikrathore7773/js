70b2b35853b854eb14a47df2df38f2f27dd9c096
kartik singh rathore <143498869+kartikrathore7773@users.noreply.github.com>
2025-07-21 05:57:19 +0000

    flowcontrol

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,21 @@
+const month = 3
+switch (month) {
+    case 1:
+        console.log("January");
+         break;
+    case 2:
+        console.log("Feb");
+         break;
+    case 3:
+        console.log("Mar");
+         break;
+    case 4:
+        console.log("April");
+         break;
+
+    default:
+        console.log("Default case matched");
+        
+        break;
+}
+
